53c4d9644c80a4044849d6d954aecacbe6ffe125
DL <dvdgege5@gmail.com>
2018-08-14 13:57:22 -0700

    add wrapper function to soupify a page

---
+++ soupify.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def soupify ( url ) :
+	"""
+	Given an URL, request the page and return a BeautifulSoup object.
+	If the request was not successful, return None
+	"""
+
+	page = requests.get(url)
+	if page.status_code >= 200 and page.status_code < 400:
+		return BeautifulSoup ( page.content, 'html.parser' )
+	else 
+		return None
