b12822f7658a3d678aa1ff6d2a9d958d63362295
DL <dvdgege5@gmail.com>
2018-08-14 16:10:50 -0700

    finish analyze_soup function

--- nfl_results.py
+++ nfl_results.py
@@ -2,6 +2,7 @@
 # week-by-week game results
 
 from soupify import soupify
+from collections import namedtuple
 
 
 
@@ -12,6 +13,21 @@ WINNER_CLASS	= 'winner'
 LOSER_CLASS		= 'loser'
 DRAW_CLASS		= 'draw'
 
+
+
+
+###############################################################################
+##### CLASS DECLARATIONS
+###############################################################################
+
+GameResult = namedtuple ( 'GameResult', 'winner loser winner_score loser_score year week' )
+
+
+
+
+###############################################################################
+##### HELPER FUNCTIONS
+###############################################################################
 
 def get_week_results ( year, week_number ) :
 	"""
@@ -29,9 +45,16 @@ def get_week_results ( year, week_number ) :
 
 
 
-def analyze_soup ( soup ) :
+
+def analyze_soup ( soup, yr=0, wk=0 ) :
+	"""
+	Given the soup of a PFR weekly results page, analyze the games that happened in the week.
+	Return a list of namedtuples, containing the results of each game
+	"""
+
 	# get a list of every game
 	all_games = soup.find_all ( 'div', class_=CONTAINER_CLASS )
+	analyzed_games = []
 
 	# for each game in the list, get the winner, loser, score
 	for game in all_games:
@@ -45,26 +68,34 @@ def analyze_soup ( soup ) :
 		# assuming each game has 1 winner and 1 loser... if there is a winner and loser
 		if winner and loser :
 			winner_str_list = winner.get_text().split('\n')
-			winner_name = winner_str_list[1]
-			winner_score = winner_str_list[2]
-			# print ( winner_str_list )
 			loser_str_list = loser.get_text().split('\n')
-			loser_name = loser_str_list[1]
-			loser_score = loser_str_list[2]
-			print ( loser_name )
+			game_result = GameResult (		# create a namedtuple with the following properties
+				winner_str_list[1],				# name of winner
+				loser_str_list[1],				# name of loser
+				winner_str_list[2],				# score of winner
+				loser_str_list[2],				# score of loser
+				year=yr, week=wk				# year and week numbers
+			)
+			analyzed_games.append(game_result)	# add this game to the list of analyzed games
+		# if there was no winner/loser (e.g. ended in a draw), it will not be added to analyzed_games
+
+	return analyzed_games
 
 
 
 
 
+
+
+###############################################################################
+##### MAIN
+###############################################################################
 
 def process_week ( year, week_number ):
 	# begin by making soup out of the relevant Pro Football Reference page
 	soup = get_week_results ( year, week_number )
 
 	# now that we have a page with all the match results of a given week, analyze it
-	res = analyze_soup ( soup )
-
-
+	res = analyze_soup ( soup, year, week_number )
 
 process_week ( YEAR, 1 ) 
