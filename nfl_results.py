85db9ca2795724e4048f4baf55bc47e8a9adbc73
DL <dvdgege5@gmail.com>
2018-08-14 14:17:56 -0700

    add function to get soup of PFR weekly results

--- nfl_results.py
+++ nfl_results.py
@@ -3,3 +3,30 @@
 
 from soupify import soupify
 
+
+
+BASE_URL 		= 'https://www.pro-football-reference.com/years/'
+YEAR 			= 2017
+
+def get_week_results ( year, week_number ) :
+	"""
+	Build the request URL, and make the request
+	"""
+
+	url = BASE_URL + '{}/week_{}.html'.format(year, week_number)
+	print ( 'requesting {}'.format(url) )
+	soup = soupify ( url )
+	if soup is None:
+		print ('Error: page request unsuccessful. Exiting script.')
+		exit(1)
+	else:
+		return soup
+
+
+
+def process_week ():
+	pass
+
+
+
+get_week_results ( YEAR, 1 ) 
